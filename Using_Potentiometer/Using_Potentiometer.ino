a93599f3ce1f7cb586ed835485ccdb367340152d
Harikrishnan M <90817926+The-Kriz@users.noreply.github.com>
2022-09-23 00:33:14 +0530

    Update Using_Potentiometer.ino

--- Using_Potentiometer/Using_Potentiometer.ino
+++ Using_Potentiometer/Using_Potentiometer.ino
@@ -1,1 +1,24 @@
+//Contrlling Robotic Arm using Potentiometer
+
+//Importing library
+#include <Servo.h>
+
+//Servo
+#define Servo1 3
+#define Servo2 5
+#define Servo3 6
+#define Servo4 9
+Servo myservo_1,myservo_2,myservo_3,myservo_4;
+
+//Potentiometer pins
+#define poten1 A1
+#define poten2 A2
+#define poten3 A3
+#define poten4 A4
+
+// Potentiometer values
+int Servo_val; 
+int Servo_va2;
+int Servo_va3;
+int Servo_va4;
 
