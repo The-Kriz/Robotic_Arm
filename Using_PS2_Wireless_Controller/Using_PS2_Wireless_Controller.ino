2866700a14778c4126ed836e3a808a205af4fe90
Harikrishnan M <90817926+The-Kriz@users.noreply.github.com>
2022-09-24 18:54:45 +0530

    Update Using_PS2_Wireless_Controller.ino

--- Using_PS2_Wireless_Controller/Using_PS2_Wireless_Controller.ino
+++ Using_PS2_Wireless_Controller/Using_PS2_Wireless_Controller.ino
@@ -3,3 +3,30 @@
 //Importing library
 #include <Servo.h>
 #include <PS2X_lib.h>    
+
+//Servo
+#define Servo1 3
+#define Servo2 5
+#define Servo3 6
+#define Servo4 9
+Servo myservo_1,myservo_2,myservo_3,myservo_4;
+
+//PS2 pins
+PS2X ps2x; 
+#define  ps2_clock 13
+#define  ps2_command 11
+#define  ps2_attention 10
+#define  ps2_data 12
+#define  ps2_Pressures true
+#define  ps2_Rumble true
+
+// variables 
+int error = 0; 
+byte type = 0;
+byte vibrate = 0;
+
+int Servo_val1 = 90 ; 
+int Servo_val2 = 90 ;
+int Servo_val3 = 90 ;
+int Servo_val4 = 90 ;
+
