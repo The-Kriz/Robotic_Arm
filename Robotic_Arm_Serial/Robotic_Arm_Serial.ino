cce41a2301e708ec94e5c6ff06b0c3115233b790
Harikrishnan M <90817926+The-Kriz@users.noreply.github.com>
2022-10-10 21:46:29 +0530

    Update Robotic_Arm_Serial.ino

--- Robotic_Arm_Serial/Robotic_Arm_Serial.ino
+++ Robotic_Arm_Serial/Robotic_Arm_Serial.ino
@@ -36,3 +36,38 @@ void setup()
   myservo_4.attach(Servo4);
 
 }
+
+void loop() 
+{
+
+  if (Serial.available() > 0) {
+    // read the incoming byte:
+    byte  incoming  = Serial.read();
+
+    // say what you got:
+    Serial.print("I received: ");
+    Serial.println(incoming);
+    int incomingByte = (int(incoming)-48);
+    Serial.println("incomingByte:"+String(incomingByte));
+    data = data*10+incomingByte;
+    Serial.println("data:"+String(data));
+    count +=1;
+    Serial.println("count:"+String(count));
+  }
+  if (count == 4)
+{
+  Serial.println("inside if");
+  if( (incomingByte%10) == 1)
+  {
+      Serial.println(incomingByte);
+      incomingByte = incomingByte/10;
+      Serial.println(incomingByte);
+      if(incomingByte > 1 && incomingByte < 180)
+      { 
+      myservo_1.write(incomingByte);
+      Serial.println("SERVO 1 Location :"+String(incomingByte));
+      }
+      Serial.println(incomingByte);
+      incomingByte = 0;
+      Serial.println(incomingByte);
+  }
