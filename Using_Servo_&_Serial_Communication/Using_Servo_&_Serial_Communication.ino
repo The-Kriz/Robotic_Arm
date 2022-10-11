0b6ae951439ac799a025aaf82c2e6278746f1ceb
Harikrishnan M <90817926+The-Kriz@users.noreply.github.com>
2022-10-11 11:39:44 +0530

    Update Using_Servo_&_Serial_Communication.ino

--- Using_Servo_&_Serial_Communication/Using_Servo_&_Serial_Communication.ino
+++ Using_Servo_&_Serial_Communication/Using_Servo_&_Serial_Communication.ino
@@ -37,3 +37,61 @@ void setup()
   myservo_4.attach(Servo4);
 
 }
+
+
+void loop() 
+{
+if (Serial.available() > 0) {
+    // read the incoming byte:
+    int  val = Serial.parseInt();
+
+    // say what you got:
+    Serial.print("received input: ");
+    Serial.println(val);
+    unit = val%10;
+    val = val/10;
+    Serial.println(unit);
+    Serial.println(val);
+    
+if (unit != 0 && val != 0)
+{
+  Serial.println("first if");
+  if( unit == 1)
+  {
+      Serial.println("unit 1 if");
+      if(val >= 1 && val <= 179)
+      { 
+      myservo_1.write(val);
+      Serial.println("SERVO 1 Location :"+String(val));
+      }
+      else
+      {
+      Serial.println("Enter a position value between 0 and 180");
+      }
+  }
+  else if( unit == 2)
+  {
+    Serial.println("unit 2 if");
+      if(val >= 1 && val <= 179)
+      { 
+      myservo_2.write(val);
+      Serial.println("SERVO 2 Location :"+String(val));
+      }
+      else
+      {
+      Serial.println("Enter a position value between 0 and 180");
+      }
+  }
+  else if( unit == 3)
+  {
+    Serial.println("unit 3 if");
+      if(val >= 1 && val <= 179)
+      { 
+      myservo_3.write(val);
+      Serial.println("SERVO 3 Location :"+String(val));
+      }
+      else
+      {
+      Serial.println("Enter a position value between 0 and 180");
+      }
+  }
