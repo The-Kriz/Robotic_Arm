6a82cb86879a3b7fcb03c7185bc0ad1e31c4e3f3
Harikrishnan M <90817926+The-Kriz@users.noreply.github.com>
2022-10-17 21:55:41 +0530

    Update 2DOF_Planar_Forward_kinematics_servo.ino

--- 2DOF_Planar_Forward_kinematics_servo/2DOF_Planar_Forward_kinematics_servo.ino
+++ 2DOF_Planar_Forward_kinematics_servo/2DOF_Planar_Forward_kinematics_servo.ino
@@ -34,3 +34,39 @@ void setup()
   linkLength_2 = Serial.parseFloat();
 
 }
+
+
+void loop()
+{
+  Serial.println("Enter the angle 1 in degree ");
+  while (Serial.available() == 0) {}
+  angle_1 = Serial.parseFloat();
+
+  Serial.println("Enter the angle 2 in degree ");
+  while (Serial.available() == 0) {}
+  angle_2 = Serial.parseFloat();
+
+  radAngle_1 = (angle_1 * pi) / 180;
+  radAngle_2 = (angle_2 * pi) / 180;
+
+  myservo_1.write(angle_1);
+  myservo_2.write(angle_2);
+
+  endEffector_X = linkLength_1 * cos(radAngle_1) + linkLength_2 * cos(radAngle_1 + radAngle_2);
+  endEffector_Y = linkLength_1 * sin(radAngle_1) + linkLength_2 * sin(radAngle_1 + radAngle_2);
+  delay(1000);
+  Serial.print("L1 =  ");
+  Serial.println(linkLength_1);
+  Serial.print("L2 = ");
+  Serial.println(linkLength_2);
+  Serial.print("x = ");
+  Serial.println(endEffector_X);
+  Serial.print("y = ");
+  Serial.println(endEffector_Y);
+  Serial.print("angle1 is ");
+  Serial.println(angle_1);
+  Serial.print("angle2 is ");
+  Serial.println(angle_2);
+  
+  delay(500);
+}
